e2ac6b8982003d4359e14b881081356c8cfe5cc7
Faaizah Afoda <fafoda@fafoda-ltmu7zq.internal.salesforce.com>
2023-07-18 13:59:17 -0700

    Profile Page started

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,10 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {Link} from 'react-router-dom'
 
 const Profile = () => {
+  const [profilePic, setProfilePic]= useState(null)
+
+
+
+    const handlePictureUpload = (event)=>{
+
+      const file = event.target.value[0]
+      setProfilePic(file)
+    }
   return (
-    <div>Profile</div>
-  )
-}
+
+    <div>
+         <h1>Profile</h1>
+
+         <div>
+
+            <h2>Profile Picture</h2>
+                    <input type="file" accept="image/*" capture="camera" onChange={handlePictureUpload} />
+
+                      {profilePic && (
+                        <img src={URL.createObjectURL(profilePic)} alt="Profile" />
+                      )}
+                          {/* <h2>Profile Picture</h2>
+                          <input type="file" onChange={handlePictureUpload} />
+
+                          {profilePic && (
+                            <img src={URL.createObjectURL(profilePic)} alt="Profile" />
+                          )}
+                        </div> */}
+
+                  <div>
+                  
+                  
+                      <h2>Navigation</h2>
+
+
+                          <ul>
+                            <p>
+                              <Link to="/favorites">Favorites</Link>
+                            </p>
+                            <p>
+                              <Link to="/recipe-book">Recipe Book</Link>
+                            </p>
+                            <p>
+                              <Link to="/grocery-list">Grocery List</Link>
+                            </p>
+                          </ul>
+                    </div>
+            </div>
+    </div>
+
+  );
+};
+
 
 export default Profile
 
