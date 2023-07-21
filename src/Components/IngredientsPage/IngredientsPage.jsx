179fe400a6dd17d84967b28b51914a4d03090c88
blessingadomakoh <badomakoh02@gmail.com>
2023-07-21 15:17:42 -0700

    changes

--- src/Components/IngredientsPage/IngredientsPage.jsx
+++ src/Components/IngredientsPage/IngredientsPage.jsx
@@ -4,103 +4,67 @@ import queryString from 'query-string';
 import { Typography, Box } from '@mui/material';
 import { Link } from 'react-router-dom';
 import "./IngredientsPage.css"
-import { react } from '@vitejs/plugin-react';
-
-
-
 const IngredientsPage = () => {
-
-// using State to handle the changing values of ingredients and the input box 
+// using State to handle the changing values of ingredients and the input box
   const [ingredients, setIngredients]= useState([]);
   const [inputValue, setInputValue]= useState(" ");
-
-
   // Initialize state for the selected cuisine
-
-
-
   const [selectedCuisine, setSelectedCuisine] = useState("");
-
   // Get the location object
   const location = useLocation();
-
   // Use useEffect to parse the query string whenever the location changes
   useEffect(() => {
     const values = queryString.parse(location.search);
     setSelectedCuisine(values.cuisine);
   }, [location]);
-
-
-
-
   const maxIngredients = 5;
-
-
-  //changes value (inputValue) of the input field when a user puts in something different 
+  //changes value (inputValue) of the input field when a user puts in something different
   const handleInputValue = (e)=>{
     setInputValue(e.target.value);
   }
-
-  //using spreader so that the previous ingredient entered in the array and populated 
-  //while the field input is set back to an empty string 
+  //using spreader so that the previous ingredient entered in the array and populated
+  //while the field input is set back to an empty string
   const HandleAddIngredient = (e)=>{
-
     if (ingredients.length >= maxIngredients) {
       alert("You cannot enter more than 5 ingredients.");
       return;
     }
-
-    if (ingredients.length < 5 && inputValue.trim() !== "") 
+    if (ingredients.length < 5 && inputValue.trim() !== "")
     { setIngredients([...ingredients, inputValue]);
     setInputValue('')};
-
   }
-
   const handleKeyPress = (e) => {
     if (e.key === "Enter") {
       HandleAddIngredient();
     }
   };
   return (
-
-    <div> 
+    <div>
         <img src="../images/ceramic bowl.png" className="bowl-image" alt="Bowl" />
-
       <Box sx={{ position: 'absolute', top: '150px', left: '150px' }}>
-        <Typography variant="h5" sx={{ color: 'darkslategray', fontFamily: 'Italiana' }}>
+        <Typography variant="h5" sx={{ color: 'darkslategray', fontFamily: 'Italiana', fontSize: '30px' }}>
           {`Chosen Cuisine: ${selectedCuisine || ''}`}
         </Typography>
       </Box>
-
-
-     
-
-        <div className="return"> 
- 
-    <div className="form-container"> 
-
-        <input 
-
+        <div className="return">
+    <div className="form-container">
+        <input
           type= "type"
           value= {inputValue}
           onChange= {handleInputValue}
           onKeyDown={handleKeyPress}
           placeholder="Enter an Ingredient"
           />
-
 {/* after button is clicked user can put another ingredient  */}
-          <button onClick={HandleAddIngredient}> 
-          Enter Ingredient
-          </button>
+          <button onClick={HandleAddIngredient}>
+            STIR
+            </button>
           </div>
-
+          <h5 className="ingredient_title">Ingredients</h5>
           <Link to="/recipe-result">
              <button>MIX</button>
           </Link>
-
-          <div className="ingredients-list"> 
-
-{/*  */}
+          <div className="ingredients-list">
           <ul>
             {ingredients.map((ingredients, index) =>(
             <p key ={index}> {ingredients} </p> ))}
@@ -110,8 +74,4 @@ const IngredientsPage = () => {
     </div>
   )
 }
-
 export default IngredientsPage;
-
-
-//the ingredients that the user lsits that they need 
