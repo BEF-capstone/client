6c88fd28bf9d86df6221137bb475856f3538c791
Faaizah Afoda <fafoda@fafoda-ltmu7zq.internal.salesforce.com>
2023-08-02 16:10:55 -0700

    Button aligned page

--- src/Components/Info/Info.jsx
+++ src/Components/Info/Info.jsx
@@ -1,68 +1,53 @@
-import React from 'react'
+import React from "react";
 import { Box, Grid, TextField, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
-import "./Info.css"
+import "./Info.css";
 
-const Info = ({handleSubmit}) => {
-
+const Info = ({ handleSubmit }) => {
   return (
     <>
- {/* <div className='banner3'>
- <p className="erasing-animation">More Eating. Less thinking.</p>
- </div> */}
+      <Box
+        sx={{
+          backgroundColor: "#5E0B15",
+          minHeight: "30vh",
+          width: "100%",
+          px: 2,
+          display: "flex",
+          justifyContent: "space-around",
+          flexWrap: "wrap",
+          padding: 5,
+        }}
+      >
+        <div className="About">
+          <h1 className="text"> About </h1>
+          <p className="ptext">
+            Get ready to actually enjoy the meals you cook.
+          </p>
+          <p className="ptext"> No more fustration with Chef Compass.</p>
+          <p className="ptext"> Lets prioritize the experience </p>
 
- <Box     
-      sx={{
-      backgroundColor: "#5E0B15",
-      minHeight: "30vh",
-      width: "100%",
-      px: 2,
-      display: 'flex',
-      justifyContent: 'space-around',
-      flexWrap: 'wrap',
-      padding: 5
-       // Add horizontal padding
-    }}
->
-
-<div className="About">
-            <h1 className='text'> About </h1>
-            <p className='ptext'> Get ready to actually enjoy the meals you cook.  </p>
-            <p className='ptext'> No more fustration with Chef Compass.</p>
-            <p className='ptext'>  Lets prioritize the experience </p>
-
-            <button> Learn </button>
-
-  </div>
-        <div className= "Get-Started">
-            <h1 className='text'> Get Started </h1>
-            <p className='ptext'> 1. Select a Cuisine of your choice!</p>
-            <p className='ptext'> 2. Input your ingredients</p>
-            <p className='ptext'> 3. Click stir to get started</p>
-            <Link to="/authenticate" onClick={handleSubmit}>
-        <button>MIX</button>
-      </Link>
-
-        </div>
-        <div className= "Recipes">
-            <h1 className='text'> Recipes </h1>
-            <p className='ptext'> Our aim is to satisty the taste buds of  </p>
-            <p className='ptext'>  the most diverse palettes. </p>
-
-            <button> Explore </button>
+          <button> Learn </button>
         </div>
 
+        <div className="Get-Started">
+          <h1 className="text"> Get Started </h1>
+          <p className="ptext"> 1. Select a Cuisine of your choice!</p>
+          <p className="ptext"> 2. Input your ingredients</p>
+          <p className="ptext"> 3. Click stir to get started</p>
+          <Link to="/authenticate" onClick={handleSubmit}>
+            <button>MIX</button>
+          </Link>
+        </div>
+        <div className="Recipes">
+          <h1 className="text"> Recipes </h1>
+          <p className="ptext"> Our aim is to satisty the taste buds of </p>
+          <p className="ptext"> the most diverse palettes. </p>
 
-
-
- </Box>
-
-    {/* <div className='Info'>
-
-        
-    </div> */}
+          <button className= "Explore-button"> Explore </button>
+        </div>
+      </Box>
     </>
-  )
-}
+  );
+};
 
-export default Info
+export default Info;
