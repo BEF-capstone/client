7ea8b9966e2689b9b318fb8c2d9af660bbcfc4b6
blessingadomakoh <badomakoh02@gmail.com>
2023-08-07 11:03:33 -0700

    changes to pics

--- src/Components/Creators/Creators.jsx
+++ src/Components/Creators/Creators.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Card, Avatar, Typography, Grid, Box } from '@mui/material';
+import Blessing from '../../../public/Blessing.jpg'
 
 const creatorsData = [
   {
@@ -8,7 +9,7 @@ const creatorsData = [
     year: 'Junior (Class of 2025)',
     major: 'Computer Science',
     favoriteFood: 'Waakye (Ghanaian Cuisine)',
-    imageUrl: '../../../public/Blessing.jpg',
+    imageUrl: Blessing,
   },
   {
     name: 'Efren Enriquez Mendoza',
