b5c9072f8eb1984536f1efadd486c831edd9a2a3
Efren Mendoza <efrenrobotics@gmail.com>
2023-08-08 15:01:53 -0700

    backend connection

--- constants.js
+++ constants.js
@@ -5,7 +5,7 @@ const PRODUCTION_API_BASE_URL = "https://chefcompass-api.onrender.com";
 const DEVELOPMENT_API_BASE_URL = "http://localhost:3001";
 
 // const API_BASE_URL = process.env.NODE_ENV === `production` ? prodUrl : devUrl;
-const API_BASE_URL = "http://localhost:3001";
+const API_BASE_URL = "https://chefcompass-api.onrender.com";
 console.log(API_BASE_URL);
 
 export default API_BASE_URL;
